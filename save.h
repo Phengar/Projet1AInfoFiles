2a9d25d7c11c0884bef6349d3cc39883dd8fc402
TheBanji <benjamin.game15@gmail.com>
2021-11-22 18:45:36 +0100

    Added a stack module and a Data module which contains the structure of both Raw and Processed data. main.c is a test program for the implemented functions

--- save.h
+++ save.h
@@ -1,24 +1,12 @@
-// Raw data structure
-typedef struct {
-	int id; // Customer's id
-	int day; // Day
-	int at; // Arrival time
-	int st; // Service begin time
-	int et; // End time
+#ifndef SAVE_H_ // To avoid multiple including
+#define SAVE_H_
 
-} Raw;
+#include "stack.h"
 
-// Processed data structure
-typedef struct {
-	int mean_s; // Queue mean size
-	int max_s; // Queue max size
-	int client_total; // Number of client during the day
-	int angry_client; // Number of angry client
-	int mean_service_time; // Mean service time
-} Processed;
+void save_raw(char * fname, Stack ** s);
 
-void save_raw(char * fname, Raw * data, int size);
+void save_processed(char * fname, Processed * data);
 
-void save_processed(char * fname, Processed data);
+void load_raw(char * fname, Stack ** s);
 
-void load_raw(char * fname, Raw * data, int size);
+#endif
