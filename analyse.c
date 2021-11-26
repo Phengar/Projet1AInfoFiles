a020e6cc8951e6ddce7957feafc7d2f4796830c0
root <root@kali>
2021-11-26 12:28:26 -0500

    njour

--- analyse.c
+++ analyse.c
@@ -3,20 +3,27 @@
 #include <math.h>
 #include "save.h"
 
-#define path[] "raw.txt"
+#define path "raw.txt"
 
-int lecture()
 
 void main(){
 	Raw data[10000];
 	FILE *file;
-	file = fopen("./raw.txt","r");
+	file = fopen("./raw2.txt","r");
 	if (file==NULL){
-		exit("Can't open file!");
+		printf("Can't open file!");
+		return;
 	}
 	int i=0;
 	while (fscanf(file, "%d %d %d %d %d\n", &data[i].id, &data[i].day, &data[i].at, &data[i].st, &data[i].et)!=-1){
 		i++;
 	}
-	
+	int nbp = i+1;
+	int nbj = data[0].day;
+	printf("nbp= %d nbp et nbj=%d\n", nbp, nbj);
+	float nbpf = (float) nbp;
+	float nbjf = (float) nbj;
+	printf("nbpf= %f  et nbjf=%f\n", nbpf, nbjf);
+	float djm = nbpf/nbjf;
+	printf("The average number number of people per day is %f\n", djm);
 }
